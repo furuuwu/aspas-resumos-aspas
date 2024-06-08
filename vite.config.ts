ee0adf21c1c7a005fed7cdac4846ff7ef5d7ec9d
ric-msi <ricardo.filipe.saleiro.abreu@gmail.com>
2024-06-08 13:49:16 +0100

    1st

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+export default defineConfig({
+    root,
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'index.html'),
+                "geo-regional": resolve(root, 'geo-regional', 'index.html'),
+            }
+        }
+    },
+    base: '/aspas-resumos-aspas/',
+})
