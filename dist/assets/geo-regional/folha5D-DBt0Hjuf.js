3231a57f22604cb5a0296602ff35d2a6ef8400d4
Your Name <you@example.com>
2024-06-09 20:37:41 +0100

    code isn't great that's for sure

---
+++ dist/assets/geo-regional/folha5D-DBt0Hjuf.js
@@ -0,0 +1,1 @@
+import"../modulepreload-polyfill-B5Qt9EMX.js";import{d as o}from"../common-Dx7LxLht.js";import"../zoomist-BqW25zLG.js";const m="folha5D.jpg";o(m);
---
+++ dist/assets/main-Bj2pU4by.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";const s="/aspas-resumos-aspas/assets/studying--dNe017O.jpg",t=document.getElementById("img-study");t.src=s;
