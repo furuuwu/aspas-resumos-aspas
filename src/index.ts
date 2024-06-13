09a280a1002547e7b076b9af3ca1ed030200253b
ric-msi <ricardo.filipe.saleiro.abreu@gmail.com>
2024-06-13 19:08:55 +0100

    that's all..

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@
 import "./style.css";
 
 // most of the time, there is no point in doing it like this (just refreshing my memory)
-import study_img_url from './public/studying.jpg'
+import study_img_url from './studying.jpg'
 const study_img = document.getElementById('img-study')! as HTMLImageElement;
 study_img.src = study_img_url;
